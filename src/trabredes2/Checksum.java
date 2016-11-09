c41d61065419fcf5465f1a0b9dbbff417c8b3267
Caio Serra <caio_mello@id.uff.br>
2016-11-08 23:28:35 -0200

    resolvendo legibilidade das variaveis

--- src/trabredes2/Checksum.java
+++ src/trabredes2/Checksum.java
@@ -44,74 +44,77 @@ public class Checksum{
 	        }
 	    }
 	    
-	public static int[] somaC1(int[] pt1, int[] pt2){
-        int[] temp = new int[8];         
-        int[] vet1 = {0,0,0,0,0,0,0,1};
+	public static int[] somaC1(int[] pt1Msg, int[] pt2Msg){
+        int[] resultadoSomaC1 = new int[8];         
+        int[] vetorUnitarioSomaCarry = {0,0,0,0,0,0,0,1};
         int result = 0;
         int carry = 0;
         int valor =0;
         for(int i=7;i>=0;i--){
-            valor = pt1[i] + pt2[i] + carry;
+            valor = pt1Msg[i] + pt2Msg[i] + carry;
             result = getResult(valor);
             carry = getCarry(valor);
-            temp[i] = result;
+            resultadoSomaC1[i] = result;
 
             if(i==0 && carry == 1){
                 result = 0;
                 carry = 0;
                 valor =0;
                 for(int j=7;j>=0;j--){
-                    valor = temp[j] + vet1[j] + carry;
+                    valor = resultadoSomaC1[j] + vetorUnitarioSomaCarry[j] + carry;
                     result = getResult(valor);
                     carry = getCarry(valor);
-                    temp[j] = result;
+                    resultadoSomaC1[j] = result;
                 }
             }
         }
-        return temp;
+        return resultadoSomaC1;
     }
 	
 	public static int[] executa (int[] msg) {
 		int[] valorChecksum = new int[8];
-        int[] pt1 = new int[8];
-        int[] pt2 = new int[8];
-        int[] resultado = new int[8];
-        int tam = pt1.length + pt2.length;
+        int[] pt1Msg = new int[8];
+        int[] pt2Msg = new int[8];
+        int[] resSomaC1 = new int[8];
+        int nBitsUtilizados = pt1Msg.length + pt2Msg.length;
         int tamMsg = msg.length;
         int j=0;
+        //Esse loop armazena os valores referentes aos 2 bytes finais da mensagem
+        //formando o primeiro passo do cálculo de Checksum
+        //i = 8º byte menos significativo j= índice das partes da mensagem. 
         for (int i=tamMsg-8;i<tamMsg;i++){
-            pt1[j] = msg[i];
-            pt2[j] = msg[i-8];
+            pt1Msg[j] = msg[i];
+            pt2Msg[j] = msg[i-8];
             j++;
         }
         System.out.println("");
         System.out.print("1: ");
-        imprimeVetor(pt1);
+        imprimeVetor(pt1Msg);
         System.out.print("2: ");
-        imprimeVetor(pt2);
-        resultado = somaC1(pt1,pt2);
+        imprimeVetor(pt2Msg);
+        resSomaC1 = somaC1(pt1Msg,pt2Msg);
         System.out.print("3: ");
-        imprimeVetor(resultado);
+        imprimeVetor(resSomaC1);
         System.out.println("");
-        while(tam<tamMsg){
+        while(nBitsUtilizados<tamMsg){
            j=0;
             while(j<8){
-                pt2[j] = msg[tamMsg-(tam+8)+j];
+                pt2Msg[j] = msg[tamMsg-(nBitsUtilizados+8)+j];
                 j++;
             }
-            tam+=8;
+            nBitsUtilizados+=8;
             System.out.print("1: ");
-            imprimeVetor(resultado);
-            resultado = somaC1(resultado,pt2);
+            imprimeVetor(resSomaC1);
+            resSomaC1 = somaC1(resSomaC1,pt2Msg);
             System.out.print("2: ");
-            imprimeVetor(pt2);
+            imprimeVetor(pt2Msg);
             System.out.print("3: ");
-            imprimeVetor(resultado);
+            imprimeVetor(resSomaC1);
             System.out.println("");
         }
 
         for(int i=0;i<8;i++){
-            valorChecksum[i] = resultado[i] == 0? 1 : 0;
+            valorChecksum[i] = resSomaC1[i] == 0? 1 : 0;
         }
         return valorChecksum;
 	}
